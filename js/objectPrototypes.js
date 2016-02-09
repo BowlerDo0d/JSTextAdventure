7b3cb0f783de03245e2c93230f3459c135f0bbb3
Steve Mahony <smahony22@gmail.com>
2016-02-08 21:31:15 -0500

    Basic game object prototypes

---
+++ js/objectPrototypes.js
@@ -0,0 +1,38 @@
+var game = {
+  // Functions save game, load game, start new game
+  // Array of players
+  // Map object
+};
+
+var map = {
+  // Array of {room}s
+};
+
+var room = {
+  // Description
+};
+
+var player = {
+  // Name, HP, attack power
+  // Inventory
+  // Position on map
+};
+
+var enemy = {
+  // Name, type, HP, attack power
+  // Inventory
+};
+
+var item = {
+  // Name, type
+};
+
+var inventory = {
+  // Array of {item}s
+  // Add, Delete, List functions
+};
+
+var dictionary = {
+  // Array of possible words
+  // Lookup function
+};
